e61825b59db8d55788e38dbb6da7c2641de7e685
Damien Caro <dcaro@Damiens-MacBook-Pro.local>
2018-02-09 12:56:07 -0800

    Adding .kitchen and test submodule

---
+++ modules/test/main.tf
@@ -0,0 +1,18 @@
+module "test-nsg" { 
+    source = "../http"
+
+    security_group_name = "web-http"
+    resource_group_name = "nsg-http"
+    location = "westus"
+
+    destination_address_prefix = ["10.0.3.0/32"]
+    
+}
+
+output "nsg_id" {
+    value = "${module.test-nsg.network_security_group_id}"
+}
+
+output "nsg_name" {
+    value = "${module.test-nsg.network_security_group_name}"
+}
