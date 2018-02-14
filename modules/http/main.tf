2c56da2b8a8140fb235e0e15d19e7fbd2af1755a
Damien Caro <dcaro@Damiens-MacBook-Pro.local>
2018-02-14 10:06:34 -0800

    added http module

--- modules/http/main.tf
+++ modules/http/main.tf
@@ -1,12 +1,14 @@
 module "nsg" {
-  source                     = "../../"
-  resource_group_name        = "${var.resource_group_name}"
-  location                   = "${var.location}"
-  security_group_name        = "${var.security_group_name}"
-  predefined_rules           = { 
-                                 http-80-tcp = "120"
-                                 ssh = "300"
-                               } 
+  source              = "../../"
+  resource_group_name = "${var.resource_group_name}"
+  location            = "${var.location}"
+  security_group_name = "${var.security_group_name}"
+
+  predefined_rules = [{
+    name     = "HTTP"
+  },
+  ]
+  custom_rules               = "${var.custom_rules}"
   source_address_prefix      = "${var.source_address_prefix}"
   destination_address_prefix = "${var.destination_address_prefix}"
   tags                       = "${var.tags}"
--- test/integration/fixtures/output.tf
+++ test/integration/fixtures/output.tf
@@ -1,3 +1,7 @@
 output "network_security_group_id" {
   value = "${module.network-security-group.network_security_group_id}"
 }
+
+# output "test"{
+#   value = "${module.network-security-group.test}"
+# }
