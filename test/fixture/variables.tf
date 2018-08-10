fa251cc49073fa567ee057748a4f2a6696898f0a
Zhenchen Xiao <t-zhxia@microsoft.com>
2018-08-10 17:05:21 +0800

    Add Dockerfile and Terratest

---
+++ test/fixture/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {}
+
+variable "custom_rules" {
+  type = "list"
+}
+
+variable "predefined_rules" {
+  type = "list"
+}
---
+++ test/terrraform_nsg_test.go
@@ -0,0 +1,67 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/gruntwork-io/terratest/modules/test-structure"
+)
+
+func TestTerraformNSG(t *testing.T) {
+	t.Parallel()
+
+	fixtureFolder := "./fixture"
+
+	// Deploy the example
+	test_structure.RunTestStage(t, "setup", func() {
+		terraformOptions := configureTerraformOptions(t, fixtureFolder)
+
+		// Save the options so later test stages can use them
+		test_structure.SaveTerraformOptions(t, fixtureFolder, terraformOptions)
+
+		// This will init and apply the resources and fail the test if there are any errors
+		terraform.InitAndApply(t, terraformOptions)
+	})
+
+	// Check whether the length of output meets the requirement
+	test_structure.RunTestStage(t, "validate", func() {
+		terraformOptions := test_structure.LoadTerraformOptions(t, fixtureFolder)
+
+		testPredefinedHTTP := terraform.Output(t, terraformOptions, "network_security_group_id_testPredefinedHTTP")
+		testPredefinedAD := terraform.Output(t, terraformOptions, "network_security_group_id_testPredefinedAD")
+		testPredefinedWithCustom := terraform.Output(t, terraformOptions, "network_security_group_id_testPredefinedWithCustom")
+		testCustom := terraform.Output(t, terraformOptions, "network_security_group_id_testCustom")
+		if len(testPredefinedHTTP) <= 0 {
+			t.Fatal("Wrong output for testPredefinedHTTP")
+		}
+		if len(testPredefinedAD) <= 0 {
+			t.Fatal("Wrong output for testPredefinedAD")
+		}
+		if len(testPredefinedWithCustom) <= 0 {
+			t.Fatal("Wrong output for testPredefinedWithCustom")
+		}
+		if len(testCustom) <= 0 {
+			t.Fatal("Wrong output for testCustom")
+		}
+	})
+
+	// At the end of the test, clean up any resources that were created
+	test_structure.RunTestStage(t, "teardown", func() {
+		terraformOptions := test_structure.LoadTerraformOptions(t, fixtureFolder)
+		terraform.Destroy(t, terraformOptions)
+	})
+
+}
+
+func configureTerraformOptions(t *testing.T, fixtureFolder string) *terraform.Options {
+
+	terraformOptions := &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: fixtureFolder,
+
+		// Variables to pass to our Terraform code using -var options
+		Vars: map[string]interface{}{},
+	}
+
+	return terraformOptions
+}
