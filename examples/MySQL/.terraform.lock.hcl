07962b1ec63da0ef9d2dba6c0360f325a7c7de92
Pumpkin-3906 <anna88x88@gmail.com>
2022-12-15 23:04:22 -0600

    delete unnecessary tests and files; modify gitignore

--- examples/MySQL/.terraform.lock.hcl
+++
@@ -1,9 +0,0 @@
-# This file is maintained automatically by "terraform init".
-# Manual edits may be lost in future updates.
-
-provider "registry.terraform.io/hashicorp/azurerm" {
-  version = "3.35.0"
-  hashes = [
-    "h1:UgjTFMinFpuXO3brreeB8B0ZucSaPyeHdPAgUqHTANs=",
-  ]
-}
--- test/e2e/terraform_network_security_group_test.go
+++ test/e2e/terraform_network_security_group_test.go
@@ -13,32 +13,6 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-func TestCassandra(t *testing.T) {
-	test_helper.RunE2ETest(t, "../../", "examples/Cassandra", terraform.Options{
-		Upgrade: true,
-	}, func(t *testing.T, output test_helper.TerraformOutput) {
-		_, exists := output["network_security_group_id"]
-		if exists {
-			vnetId, ok := output["network_security_group_id"].(string)
-			assert.True(t, ok)
-			assert.Regexp(t, regexp.MustCompile("/subscriptions/.+/resourceGroups/.+/providers/Microsoft.Network/networkSecurityGroups/.+"), vnetId)
-		}
-	})
-}
-
-func TestFixture(t *testing.T) {
-	test_helper.RunE2ETest(t, "../../", "examples/Fixture", terraform.Options{
-		Upgrade: true,
-	}, func(t *testing.T, output test_helper.TerraformOutput) {
-		_, exists := output["network_security_group_id"]
-		if exists {
-			vnetId, ok := output["network_security_group_id"].(string)
-			assert.True(t, ok)
-			assert.Regexp(t, regexp.MustCompile("/subscriptions/.+/resourceGroups/.+/providers/Microsoft.Network/networkSecurityGroups/.+"), vnetId)
-		}
-	})
-}
-
 func TestExamples(t *testing.T) {
 	directories, err := os.ReadDir("../../examples")
 	if err != nil {
