b4ef53bc67b1ae37fee9a9d69c3971063e8a5fe5
Pumpkin-3906 <anna88x88@gmail.com>
2023-01-09 17:22:41 -0600

    enable rules in .tflint_alt.hcl

--- versions.tf
+++ versions.tf
@@ -1,3 +1,9 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">=1.2"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.11.0, < 4.0"
+    }
+  }
 }
