4e0540b1721911b53925b18b5f9a8327d6794a44
lonegunmanb <lonegunmanb@hotmail.com>
2023-01-12 19:49:56 +0800

    restore modules folder (#83)
    Co-authored-by: zjhe <hezijie@microsoft.com>

---
+++ modules/Redis/output.tf
@@ -0,0 +1,7 @@
+output "network_security_group_id" {
+  value = module.nsg.network_security_group_id
+}
+
+output "network_security_group_name" {
+  value = module.nsg.network_security_group_name
+}
