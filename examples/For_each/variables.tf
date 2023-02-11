13ab2dbd1eb9d6960ef94eabf5f1471f5d9368f7
Juanjo <juanjo@generacionred.com>
2023-02-11 18:24:49 +0100

    Introducing support for NSG rules creation with for_each iteration meta-argument

---
+++ examples/For_each/variables.tf
@@ -0,0 +1,52 @@
+variable "custom_rules" {
+  description = "Custom set of security rules using this format"
+  type        = list(any)
+  default     = []
+
+  # Example:
+  # custom_rules = [{
+  # name                   = "myssh"
+  # priority               = "101"
+  # direction              = "Inbound"
+  # access                 = "Allow"
+  # protocol               = "tcp"
+  # source_port_range      = "1234"
+  # destination_port_range = "22"
+  # description            = "description-myssh"
+  #}]
+}
+
+variable "destination_address_prefix" {
+  type    = list(any)
+  default = ["*"]
+
+  # Example: ["10.0.3.0/32","10.0.3.128/32"]
+}
+
+#variable "location" {
+#  type    = string
+#  default = "westus"
+#}
+
+variable "resource_group_name" {
+  default     = "nsg_rg"
+  description = "Name of the resource group"
+}
+
+variable "security_group_name" {
+  description = "Name of the network security group"
+  default     = "nsg"
+}
+
+variable "source_address_prefix" {
+  type    = list(any)
+  default = ["*"]
+
+  # Example: ["10.0.3.0/24"]
+}
+
+variable "tags" {
+  description = "The tags to associate with your network security group."
+  type        = map(string)
+  default     = {}
+}
