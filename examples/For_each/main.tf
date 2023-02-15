35bbd22e849f11f36ff8d6154114bd4615e56110
Juanjo <juanjo@generacionred.com>
2023-02-15 06:52:07 +0100

    fix for_each example e2e test

--- examples/For_each/main.tf
+++ examples/For_each/main.tf
@@ -7,7 +7,17 @@ module "nsg" {
 
   predefined_rules = []
 
-  custom_rules               = var.custom_rules
+  custom_rules = [{
+    name                         = "myssh"
+    priority                     = 101
+    direction                    = "Inbound"
+    access                       = "Allow"
+    protocol                     = "Tcp"
+    destination_address_prefixes = ["10.151.0.0/24", "10.151.1.0/24"]
+    destination_port_range       = "22"
+    description                  = "description-myssh"
+  }]
+
   source_address_prefix      = var.source_address_prefix
   destination_address_prefix = var.destination_address_prefix
   tags                       = var.tags
--- examples/For_each/variables.tf
+++ examples/For_each/variables.tf
@@ -1,20 +1,20 @@
-variable "custom_rules" {
-  description = "Custom set of security rules using this format"
-  type        = list(any)
-  default     = []
+# variable "custom_rules" {
+#   description = "Custom set of security rules using this format"
+#   type        = list(any)
+#   default     = []
 
-  # Example:
-  # custom_rules = [{
-  # name                   = "myssh"
-  # priority               = "101"
-  # direction              = "Inbound"
-  # access                 = "Allow"
-  # protocol               = "tcp"
-  # source_port_range      = "1234"
-  # destination_port_range = "22"
-  # description            = "description-myssh"
-  #}]
-}
+#   # Example:
+#   # custom_rules = [{
+#   # name                   = "myssh"
+#   # priority               = "101"
+#   # direction              = "Inbound"
+#   # access                 = "Allow"
+#   # protocol               = "tcp"
+#   # source_port_range      = "1234"
+#   # destination_port_range = "22"
+#   # description            = "description-myssh"
+#   #}]
+# }
 
 variable "destination_address_prefix" {
   type    = list(any)
