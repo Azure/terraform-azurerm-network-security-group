f1e5786beb000f80f92dc0fd011766183ec3b6ee
hezijie <lonegunmanb@hotmail.com>
2023-05-06 14:39:35 +0800

    fix tflint issue

---
+++ examples/_template/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">=1.2"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.11.0, < 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
